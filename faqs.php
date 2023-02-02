5a7e6041030ba42ab03959deaf0de31d1b517298
White-Billa <tusharhirapara37372@gmail.com>
2023-02-02 12:58:48 +0530

    done
    done

---
+++ faqs.php
@@ -0,0 +1,194 @@
+<?php
+	include("function/login.php");
+	include("function/customer_signup.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Shoe Store</title>
+	<link rel="icon" href="img/logo.jpg" />
+	<link rel = "stylesheet" type = "text/css" href="css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery-1.7.2.min.js"></script>
+	<script src="js/carousel.js"></script>
+	<script src="js/button.js"></script>
+	<script src="js/dropdown.js"></script>
+	<script src="js/tab.js"></script>
+	<script src="js/tooltip.js"></script>
+	<script src="js/popover.js"></script>
+	<script src="js/collapse.js"></script>
+	<script src="js/modal.js"></script>
+	<script src="js/scrollspy.js"></script>
+	<script src="js/alert.js"></script>
+	<script src="js/transition.js"></script>
+	
+</head>
+<body>
+	<div id="header">
+		<img src="img/logo.jpg">
+		<label>Online Shoe Store</label>
+			<ul>
+				<li><a href="#signup"   data-toggle="modal">Sign Up</a></li>
+				<li><a href="#login"   data-toggle="modal">Login</a></li>
+			</ul>
+	</div>
+
+	<div id="login" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:400px;">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+				<h3 id="myModalLabel">Login...</h3>
+			</div>
+				<div class="modal-body">
+					<form method="post">
+					<center>
+						<input type="email" name="email" placeholder="Email" style="width:250px;">
+						<input type="password" name="password" placeholder="Password" style="width:250px;">
+					</center>
+				</div>
+			<div class="modal-footer">
+				<input class="btn btn-primary" type="submit" name="login" value="Login">
+				<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
+					</form>
+			</div>
+		</div>
+
+	<div id="signup" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:700px;">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Sign Up Here...</h3>
+				</div>
+					<div class="modal-body">
+						<center>
+					<form method="post">
+						<input type="text" name="firstname" placeholder="Firstname" required>
+						<input type="text" name="mi" placeholder="Middle Initial" maxlength="1" required>
+						<input type="text" name="lastname" placeholder="Lastname" required>
+						<input type="text" name="address" placeholder="Address" style="width:430px;"required>
+						<input type="text" name="country" placeholder="Province" required>
+						<input type="text" name="zipcode" placeholder="ZIP Code" required maxlength="4">
+						<input type="text" name="mobile" placeholder="Mobile Number" maxlength="11">
+						<input type="text" name="telephone" placeholder="Telephone Number" maxlength="8">
+						<input type="email" name="email" placeholder="Email" required>
+						<input type="password" name="password" placeholder="Password" required>
+						</center>
+					</div>
+				<div class="modal-footer">
+					<input type="submit" class="btn btn-primary" name="signup" value="Sign Up">
+					<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
+				</div>
+					</form>
+			</div>
+
+	<br>
+<div id="container">
+	<div id="menu">
+		<center>
+			 <ul>
+				<li><a href="home.php" id="home">Home</a></li>
+				<li><a href="product.php" id="product">Product</a></li>
+				<li><a href="aboutus.php" id="about">About Us</a></li>
+				<li><a href="contactus.php" id="contact">Contact Us</a></li>
+				<li><a href="privacy.php" id="privacy">Privacy Policy</a></li>
+				<li><a href="faqs.php" id="FAQs">FAQs</a></li>
+			</ul>
+		</center>
+	</div>
+<center>
+		<div id="content">
+			<!-- <legend>Frequently Added Questions</legend>
+
+				 <h4>DO YOU SHIP?</h4>
+					<p style="text-indent:60px;">Yes, we ship the products via LBC and 2GO only.</p>
+				<hr>
+					<h4>DO YOU DELIVER?</h4>
+						<p style="text-indent:60px;">No, We only offer Shipping.</p>
+				<hr>
+					<h4>WHEN WILL I GET MY ORDERS?</h4>
+						<p style="text-indent:60px; line-height:32px;">We wil ship your product 2-3 days around Negros<br> Occidental and It will take 4-6 days Nationwide.</p>
+				<hr>
+					<h4>HOW DO I PAY MY ORDERS?</h4>
+					<p style="text-indent:60px;">Through PAYPAL basis only.</p> -->
+					<div id="accordion">
+  <div class="card" style="width:80%;">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		DO YOU SHIP?
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+	  Yes, we ship the products via LBC and 2GO only.
+      </div>
+    </div>
+  </div>
+  <div class="card" style="width:80%;">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+		DO YOU DELIVER?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+	  No, We only offer Shipping.
+      </div>
+    </div>
+  </div>
+  <div class="card" style="width:80%;">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+		WHEN WILL I GET MY ORDERS?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+	  We wil ship your product 2-3 days around Negros<br> Occidental and It will take 4-6 days Nationwide.
+      </div>
+    </div>
+  </div>
+  <div class="card" style="width:80%;">
+    <div class="card-header" id="headingFour">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+		HOW DO I PAY MY ORDERS?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+      <div class="card-body">
+	  Through PAYPAL basis only.
+      </div>
+    </div>
+  </div>
+</div>
+
+		</div>
+</center>
+	<br />
+</div>
+	<br />
+	<div id="footer">
+		<div class="foot">
+			<label style="font-size:17px;"> Copyrght &copy; </label>
+			<p style="font-size:25px;">Online Shoe Store Inc. 2017 Brought To You by <a href="https://code-projects.org/">Code-Projects</a></p>
+		</div>
+
+			<div id="foot">
+				<h4>Links</h4>
+					<ul>
+						<a href="http://www.facebook.com/OnlineShoeStore"><li>Facebook</li></a>
+						<a href="http://www.twitter.com/OnlineShoeStore"><li>Twitter</li></a>
+						<a href="http://www.pinterest.com/OnlineShoeStore"><li>Pinterest</li></a>
+						<a href="http://www.tumblr.com/OnlineShoeStore"><li>Tumblr</li></a>
+					</ul>
+			</div>
+	</div>
+</body>
+</html>
